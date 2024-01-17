60c363697b7a32fff7b5e521dff6248f2ef6aa8f
Vladimir Panov <vladimirpanov73@gmail.com>
2024-01-17 17:55:34 +0300

    add code_language in back

--- codehere_back/map_objects/models.py
+++ codehere_back/map_objects/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 class MapObject(models.Model):
     username = models.CharField("Username", max_length=240)
+    code_language = models.CharField("Code language", max_length=100, db_default="")
     code = models.TextField("Code")
-    created_at = models.DateField("Creation Date", auto_now_add=True)
+    created_at = models.DateField("Creation date", auto_now_add=True)
     lon = models.FloatField("Longitude", db_default=0.0)
     lat = models.FloatField("Latitude", db_default=0.0)
 
